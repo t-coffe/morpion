90d05a672e64e440ecc261ecc1f8a2c1d9cd066c
t-coffe <tcoffe@edenschool.fr>
2024-04-19 16:08:07 +0200

    morpion

---
+++ main.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+const cases = document.querySelectorAll('.case');
+
+let circle = false ;
+let cross = true;
+
+for (let i = 0; i < cases.length; i++) {
+    cases[i].addEventListener('click', ()=> {
+        if (cross = true) {
+            
+        }
+    })
+}
