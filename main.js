719cf98c3f5b23133fbcb543e9d2898ee0f777a5
t-coffe <tcoffe@edenschool.fr>
2024-04-19 16:41:51 +0200

    cross and circle's design completed. They appear when div clicked for their player

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ import './style.css'
 
 const cases = document.querySelectorAll('.case');
 
-let circle = false ;
-let cross = true;
+
+let currentPlayer = 1;
+
 
 for (let i = 0; i < cases.length; i++) {
     cases[i].addEventListener('click', ()=> {
-        if (cross = true) {
-            
+        console.log(cases[i]);
+        if (currentPlayer === 1) {
+            const div = document.createElement('div');
+            div.classList.add('crosses');
+            cases[i].appendChild(div);
+            currentPlayer = 2
+        } else if (currentPlayer === 2) {
+            const div = document.createElement('div');
+            div.classList.add('circles');
+            cases[i].appendChild(div);
+            currentPlayer = 1;
         }
     })
 }
