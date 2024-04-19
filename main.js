5793c2629add9c4ecdd3ddf8fd2274ea75ad1953
t-coffe <tcoffe@edenschool.fr>
2024-04-19 16:58:04 +0200

    functionality to prevent players from playing twice in the same box is done

--- main.js
+++ main.js
@@ -8,17 +8,18 @@ let currentPlayer = 1;
 
 for (let i = 0; i < cases.length; i++) {
     cases[i].addEventListener('click', ()=> {
-        console.log(cases[i]);
-        if (currentPlayer === 1) {
-            const div = document.createElement('div');
-            div.classList.add('crosses');
-            cases[i].appendChild(div);
-            currentPlayer = 2
-        } else if (currentPlayer === 2) {
-            const div = document.createElement('div');
-            div.classList.add('circles');
-            cases[i].appendChild(div);
-            currentPlayer = 1;
+        if (cases[i].children.length === 0) {
+            if (currentPlayer === 1) {
+                const div = document.createElement('div');
+                div.classList.add('crosses');
+                cases[i].appendChild(div);
+                currentPlayer = 2
+            } else if (currentPlayer === 2) {
+                const div = document.createElement('div');
+                div.classList.add('circles');
+                cases[i].appendChild(div);
+                currentPlayer = 1;
+            }
         }
     })
 }
